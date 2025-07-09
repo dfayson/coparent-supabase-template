b5a47c0ac3f0254e6314b18b23dc716abd01c303
dfayson <128818434+dfayson@users.noreply.github.com>
2025-07-08 21:07:07 -0400

    Update AuthProvider.js

--- src/AuthProvider.js
+++ src/AuthProvider.js
@@ -1,5 +1,5 @@
 import { useState, useEffect, createContext, useContext } from 'react';
-import { supabase } from '../supabaseClient';
+import { supabase } from './supabaseClient';
 
 const AuthContext = createContext();
 
